e3fd36f0ae3d0b72a8959b00036e960c92c30ebc
Shehroz <raxmatshehroz@gmail.com>
2025-03-18 16:10:26 +0500

    finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,22 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Kolej',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  description: 'Kolej',
+  generator: 'Kolej',
+  openGraph: {
+    title: 'online Kolej',
+		description:
+			"Kolej - bu sizning o'qish yo'lingizni boshlash uchun eng yaxshi joy. Bizning onlayn platformamiz orqali siz o'zingizga mos ta'lim yo'nalishini tanlashingiz mumkin.",
+		type: 'website',
+    images: [
+      {
+        url: 'https://etimg.etb2bimg.com/photo/75729614.cms',
+        width: 800,
+        height: 600,
+        alt: 'Kolej',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
